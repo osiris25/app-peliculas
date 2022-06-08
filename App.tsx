011f89214284581d2c08584264f061bffed9dc58
Wendy Osiris Rivas Martinez <wrivas@nexosmedia.com>
2022-06-08 17:25:32 -0500

    aplicacion terminada

--- App.tsx
+++ App.tsx
@@ -2,13 +2,23 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigation } from './src/navigation/Navigation';
 import { FadeScreen } from './src/screens/FadeScreen';
+import { GradientProvider } from './src/context/GradientContext';
+
+
+const AppState=({children}:any)=>{
+return(
+  <GradientProvider>
+    {children}
+  </GradientProvider>
+)}
 
 const App = ()=> {
   return (
     <NavigationContainer>
-      <Navigation/>
-      {/*<FadeScreen/>*/}
-      
+      <AppState>
+        <Navigation/>
+        {/*<FadeScreen/>*/}
+      </AppState>
     </NavigationContainer>
   );
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,25 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { ActivityIndicator, Dimensions, ScrollView, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
-import ImageColors from 'react-native-image-colors';
 import { useMovies } from '../hooks/useMovies';
 import { MoviePoster } from '../components/MoviePoster';
 import { HorizontalSlider } from '../components/HorizontalSlider';
 import { GradientBackground } from '../components/GradientBackground';
 import { getImageColors } from '../helpers/getColores';
+import { GradientContext } from '../context/GradientContext';
+import { useEffect } from 'react';
 //Posicion del telefon
 const { width: windowWidth } = Dimensions.get('window');
 export const HomeScreen = () => {
   const { nowPlaying, isLoading, popular, topRated, upcoming } = useMovies();
   const { top } = useSafeAreaInsets();
+  const {setMainColors} = useContext(GradientContext);
 
   const getPosterColors = async (index:number)=>{
-    const movie=nowPlaying[index];
+    const movie = nowPlaying[index];
     const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-    const {primary, secondary}= await getImageColors(uri);
-    console.log({primary, secondary})
+    const {primary = 'green', secondary = 'purple'} = await getImageColors(uri);
+    setMainColors({primary:primary, secondary:secondary});
+    
   } 
+
+  useEffect(() => {
+   if(nowPlaying.length > 0){
+     getPosterColors(0);
+   }
+  }, [nowPlaying])
+  
 
   if (isLoading) {
     //ActivityIndicator es un load
